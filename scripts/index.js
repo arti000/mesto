13559b6c5fd2ce96b850164e82eb3e308977e930
Artem Kireev <al-mysri@yandex.ru>
2021-06-22 22:57:56 +0300

    feat: add inputValues

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,54 @@
-//Объявляем переменные
-let formOpenButtonElement = document.querySelector('.profile__open-popup');
+//Переменные, связанные со всплывающим окном
 let formElement = document.querySelector('.popup');
+let formOpenButtonElement = document.querySelector('.profile__open-popup');
 let formCloseButtonElement = formElement.querySelector('.popup__close');
-
-//Функции, делающие видимым и скрывающая всплывающее окно
-let togglePopupVisibility = function () {
-  formElement.classList.toggle('popup_opened');
-}
-
-//Функции, открывающая и закрывающая всплывающее окно после нажатия
-formOpenButtonElement.addEventListener('click', togglePopupVisibility);
-formCloseButtonElement.addEventListener('click', togglePopupVisibility);
-
-// Находим поля формы в DOM
+let formSubmitButtomElement = formElement.querySelector('.popup__submit');
 let nameInput = formElement.querySelector('.popup__input_title');
-let jobInput = formElement.querySelector('.popup__input_subtitle');// Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.popup__input_subtitle');
 
-// // Обработчик «отправки» формы, хотя пока
-// // она никуда отправляться не будет
-function formSubmitHandler(evt) {
-  evt.preventDefault();
+//Переменные, связанные со значениями в секции profile
+let nameProfile = document.querySelector('.profile__title');
+let jobProfile = document.querySelector('.profile__subtitle');
 
-  //     // Получите значение полей jobInput и nameInput из свойства value
-
-  //     // Выберите элементы, куда должны быть вставлены значения полей
-
-  //     // Вставьте новые значения с помощью textContent
+//Функции, делающие видимым и скрывающая всплывающее окно.
+const openForm = function () {
+  formElement.classList.add('popup_opened');
 }
 
-// // Прикрепляем обработчик к форме:
-// // он будет следить за событием “submit” - «отправка»
-// formElement.addEventListener('submit', formSubmitHandler);
+const closeForm = function () {
+  formElement.classList.remove('popup_opened');
+}
+
+//Функция, делающая сброс полей до первоначальных значений и скрывающая окно.
+const resetForm = function () {
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+  closeForm();
+}
+
+//Функция, скрывающая всплывающее окно при клике на затемненную область.
+const closeFormByClickOverlay = function (event) {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+  resetForm();
+}
+
+//Функция, сохраняющая новые значения и закрывающая всплывающее окно.
+const formSubmitHandler = function (evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  closeForm();
+}
+
+//Обработчики, открывающие и закрываюие всплывающее окно после нажатия.
+formOpenButtonElement.addEventListener('click', openForm);
+formCloseButtonElement.addEventListener('click', resetForm);
+formSubmitButtomElement.addEventListener('click', formSubmitHandler);
+formElement.addEventListener('click', closeFormByClickOverlay);
+document.addEventListener('keyup', function (event) {
+  if (event.code === 'Escape') {
+    resetForm();
+  }
+});
