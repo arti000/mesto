2c97e112a02ddb8a502c46f9f9ae469fa1cf8316
Artem Kireev <al-mysri@yandex.ru>
2021-06-24 00:53:27 +0300

    fix: all js

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,54 @@
-//Переменные, связанные со всплывающим окном
-let formElement = document.querySelector('.popup');
-let formOpenButtonElement = document.querySelector('.profile__open-popup');
-let formCloseButtonElement = formElement.querySelector('.popup__close');
-let formSubmitButtomElement = formElement.querySelector('.popup__submit');
-let nameInput = formElement.querySelector('.popup__input_title');
-let jobInput = formElement.querySelector('.popup__input_subtitle');
+//Переменные, связанные с попапом
+let popupElement = document.querySelector('.popup');
+let formElement = popupElement.querySelector('.popup__content')
+let popupOpenButtonElement = document.querySelector('.profile__open-popup');
+let popupCloseButtonElement = popupElement.querySelector('.popup__close');
+let nameInput = popupElement.querySelector('.popup__input_type_title');
+let jobInput = popupElement.querySelector('.popup__input_type_subtitle');
 
 //Переменные, связанные со значениями в секции profile
 let nameProfile = document.querySelector('.profile__title');
 let jobProfile = document.querySelector('.profile__subtitle');
 
-//Функции, делающие видимым и скрывающая всплывающее окно.
-const openForm = function () {
-  formElement.classList.add('popup_opened');
+//Функции, делающие видимым и скрывающая попап.
+const openPopup = function () {
+  popupElement.classList.add('popup_opened');
 }
 
-const closeForm = function () {
-  formElement.classList.remove('popup_opened');
+const closePopup = function () {
+  popupElement.classList.remove('popup_opened');
 }
 
-//Функция, делающая сброс полей до первоначальных значений и скрывающая окно.
-const resetForm = function () {
+//Функция, вставляющая значения со страницы в попап при открытии.
+const pasteValuesToPopupInputs = function () {
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
-  closeForm();
+  openPopup();
 }
 
-//Функция, скрывающая всплывающее окно при клике на затемненную область.
-const closeFormByClickOverlay = function (event) {
+//Функция, скрывающая попап при клике на затемненную область.
+const closePopupByClickOverlay = function (event) {
   if (event.target !== event.currentTarget) {
     return;
   }
-  resetForm();
+  closePopup();
 }
 
-//Функция, сохраняющая новые значения и закрывающая всплывающее окно.
-const formSubmitHandler = function (evt) {
+//Функция, сохраняющая новые значения и закрывающая попап.
+const popupSubmitHandler = function (evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  closeForm();
-}
+  closePopup();
+  }
 
 //Обработчики, открывающие и закрываюие всплывающее окно после нажатия.
-formOpenButtonElement.addEventListener('click', openForm);
-formCloseButtonElement.addEventListener('click', resetForm);
-formSubmitButtomElement.addEventListener('click', formSubmitHandler);
-formElement.addEventListener('click', closeFormByClickOverlay);
+popupOpenButtonElement.addEventListener('click', pasteValuesToPopupInputs);
+popupCloseButtonElement.addEventListener('click', closePopup);
+formElement.addEventListener('submit', popupSubmitHandler);
+popupElement.addEventListener('click', closePopupByClickOverlay);
 document.addEventListener('keyup', function (event) {
   if (event.code === 'Escape') {
-    resetForm();
+    closePopup();
   }
 });
